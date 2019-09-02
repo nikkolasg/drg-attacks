0a59d19490c1e15ca80012ff22f790c22b78771c
nikkolasg <nikkolasg@gmail.com>
2019-09-02 18:15:58 +0200

    added remove method

--- src/graph.rs
+++ src/graph.rs
@@ -10,19 +10,20 @@ use std::fmt;
 // Graph holds the parameters and the edges of the graph.
 #[derive(Debug)]
 pub struct Graph {
-    // cardinality of the graph
-    size: usize,
     // parents holds all parents of all nodes. If j = parents[i][u], then there
     // is an edge (j -> i+1), i.e. j is the parent of i. i+1 because the first
     // node 0 never have parents so the array is never filled for the first cell.
+    // The capacity of the graph is the size of the vector - Some nodes may be
+    // absent when counted, i.e. node i may not have any parent and may not be
+    // the parent of any other node. In that case, it is not included in the graph G
     parents: Vec<Vec<usize>>,
-    rng: ChaCha20Rng,
+    seed: [u8; 32],
     algo: DRGAlgo,
 }
 
 // DRGAlgo represents which algorithm can be used to create the edges so a Graph is
 // a Depth Robust Graph
-#[derive(Debug)]
+#[derive(Debug, Copy, Clone)]
 pub enum DRGAlgo {
     // BucketSample is the regular bucket sampling algorithm with degree 2
     BucketSample,
@@ -34,12 +35,10 @@ impl Graph {
     // new returns a new graph instance from the given parameters.
     // The graph's edges are not generated yet, call fill_drg to compute the edges.
     pub fn new(size: usize, seed: [u8; 32], algo: DRGAlgo) -> Graph {
-        let cha = ChaCha20Rng::from_seed(seed);
         let mut g = Graph {
-            size,
             algo,
+            seed,
             parents: Vec::with_capacity(size),
-            rng: cha,
         };
         match g.algo {
             DRGAlgo::BucketSample => g.bucket_sample(),
@@ -48,11 +47,39 @@ impl Graph {
         g
     }
 
+    // remove returns a new graph with the specified nodes removed
+    // TODO inefficient at the moment; may be faster ways.
+    fn remove(&self, nodes: Vec<usize>) -> Graph {
+        let mut out = Vec::with_capacity(self.parents.len());
+        for i in 0..self.parents.len() {
+            let parents = self.parents.get(i).unwrap();
+            let new_parents = if nodes.contains(&(i + 1)) {
+                // no parent for a deleted node
+                Vec::new()
+            } else {
+                // only take parents which are not in the list of nodes
+                parents
+                    .into_iter()
+                    .filter(|parent| !nodes.contains(parent))
+                    .map(|&p| p)
+                    .collect::<Vec<usize>>()
+            };
+            out.push(new_parents);
+        }
+
+        Graph {
+            parents: out,
+            algo: self.algo,
+            seed: self.seed,
+        }
+    }
+
     // Implementation of the first algorithm BucketSample on page 22 of the
     // porep paper : https://web.stanford.edu/~bfisch/porep_short.pdf
     // It produces a degree-2 graph which is asymptotically depth-robust.
     fn bucket_sample(&mut self) {
-        for node in 0..self.size {
+        let mut rng = self.rng();
+        for node in 0..self.parents.len() {
             let mut parents = Vec::with_capacity(2);
             match node {
                 // no parents for the first node
@@ -67,13 +94,13 @@ impl Graph {
 
                     // choose a bucket index
                     let max_bucket = (node as f32).log2().floor() as usize;
-                    let i: usize = self.rng.gen_range(1, max_bucket + 1);
+                    let i: usize = rng.gen_range(1, max_bucket + 1);
                     // get a node from that bucket, i.e. from [2^i-1, 2^i[
                     // exclusif because otherwise a parent can be the same
                     // as its child
                     let min = 1 << (i - 1);
                     let max = 1 << i;
-                    let random_parent = self.rng.gen_range(min, max);
+                    let random_parent = rng.gen_range(min, max);
                     assert!(random_parent < node);
                     parents.push(random_parent);
                 }
@@ -84,12 +111,17 @@ impl Graph {
         }
     }
 
+    fn rng(&self) -> ChaCha20Rng {
+        ChaCha20Rng::from_seed(self.seed)
+    }
+
     // Implementation of the meta-graph construction algorithm described in page 22
     // of the porep paper https://web.stanford.edu/~bfisch/porep_short.pdf
     // It produces a degree-d graph on average.
     fn meta_bucket(&mut self, degree: usize) {
+        let mut rng = self.rng();
         let m = degree - 1;
-        for node in 0..self.size {
+        for node in 0..self.parents.len() {
             let mut parents = Vec::with_capacity(degree);
             match node {
                 // no parents for the first node
@@ -111,11 +143,11 @@ impl Graph {
                         let meta_idx = node * m;
                         let max_bucket = (meta_idx as f32).log2().floor() as usize;
                         // choose bucket index {1 ... log2(idx)}
-                        let i: usize = self.rng.gen_range(1, max_bucket + 1);
+                        let i: usize = rng.gen_range(1, max_bucket + 1);
                         // choose parent in range [2^(i-1), 2^i[
                         let min = 1 << (i - 1);
                         let max = 1 << i;
-                        let meta_parent = self.rng.gen_range(min, max);
+                        let meta_parent = rng.gen_range(min, max);
                         let real_parent = meta_parent / degree;
                         assert!(meta_parent < meta_idx);
                         assert!(real_parent < node);
@@ -132,7 +164,7 @@ impl Graph {
 
 impl fmt::Display for Graph {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "G(size:{}, drg: ", self.size)?;
+        write!(f, "G(cap:{}, drg: ", self.parents.len())?;
         match self.algo {
             DRGAlgo::BucketSample => write!(f, "bucket, ")?,
             DRGAlgo::MetaBucket(d) => write!(f, "meta-bucket (degree {}), ", d)?,
@@ -157,7 +189,7 @@ mod tests {
     fn graph_new() {
         let size = 100;
         let g1 = Graph::new(size, TEST_SEED, DRGAlgo::BucketSample);
-        assert_eq!(g1.size, size);
+        assert_eq!(g1.parents.len(), 0); // no nodes generated yet
         assert_eq!(g1.parents.capacity(), size);
     }
 
@@ -199,5 +231,26 @@ mod tests {
                 );
             }
         });
+    }
+
+    #[test]
+    fn graph_remove() {
+        // graph 1 - 5 nodes
+        // 0 -> 1, 1 -> 2, 2 -> 3, 3 -> 4
+        // 0 -> 2, 2 -> 4
+        // Remove nodes 1 and 3
+        // -> final graph 0 -> 2, 2 -> 4
+        let p1 = vec![vec![0], vec![0, 1], vec![2], vec![2, 3]];
+        let g1 = Graph {
+            parents: p1,
+            seed: TEST_SEED,
+            algo: DRGAlgo::BucketSample,
+        };
+
+        let nodes = vec![1, 3];
+        let g3 = g1.remove(nodes);
+        let expected = vec![vec![], vec![0], vec![], vec![2]];
+        assert_eq!(g3.parents.len(), 4);
+        assert_eq!(g3.parents, expected);
     }
 }
